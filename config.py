1c3ad137effd0d504f7b8ac526b627a45b1b6706
mbhesam <m.b.hesam@gmail.com>
2025-05-18 00:13:04 +0330

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+PROXY_URL = os.getenv("PROXY_URL")
+PROXY_REQUEST_TIME_OUT = os.getenv("PROXY_REQUEST_TIME_OUT")
---
+++ handlers.py
@@ -0,0 +1,12 @@
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.constants import ParseMode
+# from common import (
+#
+#     )
+from telegram.ext import (
+    ConversationHandler,
+    CommandHandler,
+    MessageHandler,
+    filters, ContextTypes,
+)
+
